1b37a24a548f489b3a1dba0453182a16e767675a
Ivan Kvasnica <ivan@ikvasnica.com>
2024-07-24 23:47:09 +0200

    Create basic project structure

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <div className='app'>
+      <div className='title'>
+        <h1>Currency Converter</h1>
+      </div>
+      <div className='currency-list'>
+
+      </div>
+      <div className='currency-converter'>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
